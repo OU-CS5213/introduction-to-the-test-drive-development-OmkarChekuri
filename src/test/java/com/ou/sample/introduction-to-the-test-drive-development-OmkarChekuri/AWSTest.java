d31b41435592a03cde15a8fe0914607bfce41eba
Omkar Chekuri <swaroopvarma.chekuri@gmail.com>
2021-03-03 23:09:11 -0600

    Update AWSTest.java

--- src/test/java/com/ou/sample/introduction-to-the-test-drive-development-OmkarChekuri/AWSTest.java
+++ src/test/java/com/ou/sample/introduction-to-the-test-drive-development-OmkarChekuri/AWSTest.java
@@ -11,25 +11,25 @@ class AWSTest {
 	private int[] original={1, 2, 3};
 	AWS originalAWS;
 
-	//@Test
-	//void testGetValues() {
-	//	fail("Not yet implemented");
-	//}
+	@Test
+	void testGetValues() {
+		fail("Not yet implemented");
+	}
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		 originalAWS = new AWS(this.original);
 	}
 
-	//@Test
-	//void testSetValues() {
-	//	fail("Not yet implemented");
-	//}
+	@Test
+	void testSetValues() {
+		fail("Not yet implemented");
+	}
 
-	//@Test
-	//void testToString() {
-	//	fail("Not yet implemented");
-	//}
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
 
 	@Test
 	void testAWS() {
@@ -110,29 +110,29 @@ class AWSTest {
 		
 	
 	}
-	//@Test
-	//void testFillAndExpandWithNegative() {
-	//	int position = 1;
-	//	int numberOfTimes = -2;
-	//	
-	//	int[] org = originalAWS.getValues();
-	//	int expectedValue = org[position];
- 	//	int first = org[0];
-	//	int expected = originalAWS.getValues().length + numberOfTimes;
-	//	originalAWS.fillAndExpand(position, numberOfTimes);
-	//	int[] result = originalAWS.getValues();
-	//	assertEquals(expected, result.length);
-	//	
-	//	int a = result[1];
-	//	int b = result[2];
-	//	int c = result[3];
-	//	assertEquals(expectedValue, a);
-	//	assertEquals(expectedValue, b);
-	//	assertEquals(expectedValue, c);
-	//	 
-	//	assertEquals(first, result[0]); 
-//
-//	
-//	}
-//
+	@Test
+	void testFillAndExpandWithNegative() {
+		int position = 1;
+		int numberOfTimes = -2;
+		
+		int[] org = originalAWS.getValues();
+		int expectedValue = org[position];
+ 		int first = org[0];
+		int expected = originalAWS.getValues().length + numberOfTimes;
+		originalAWS.fillAndExpand(position, numberOfTimes);
+		int[] result = originalAWS.getValues();
+		assertEquals(expected, result.length);
+		
+		int a = result[1];
+		int b = result[2];
+		int c = result[3];
+		assertEquals(expectedValue, a);
+		assertEquals(expectedValue, b);
+		assertEquals(expectedValue, c);
+		 
+		assertEquals(first, result[0]); 
+
+	
+	}
+
 }
